24b6e402b943e782c6915ecf6e1387af752169e2
Brett Janssen <bajanssen@mail.bradley.edu>
2023-02-15 15:08:52 -0600

    build error

--- EscapeRoom/Source/EscapeRoom/Public/TopDownCharacter.h
+++ EscapeRoom/Source/EscapeRoom/Public/TopDownCharacter.h
@@ -1,29 +1,56 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "TopDownCharacter.generated.h"
 
-UCLASS()
-class ESCAPEROOM_API ATopDownCharacter : public ACharacter
+UCLASS(config = Game)
+class ATopDownCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+		/** Camera boom positioning the camera behind the character */
+		UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class UCameraComponent* FollowCamera;
 public:
-	// Sets default values for this character's properties
 	ATopDownCharacter();
 
+	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Input)
+		float TurnRateGamepad;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+		class UInputMappingContext* IC_Character;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+		class UInputAction* IA_Move;
+
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	/** Handler for when a touch input begins. */
+	void TouchStarted(ETouchIndex::Type FingerIndex, FVector Location);
 
-	// Called to bind functionality to input
+	/** Handler for when a touch input stops. */
+	void TouchStopped(ETouchIndex::Type FingerIndex, FVector Location);
+
+	void Move(const FInputActionValue& Value);
+
+protected:
+	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// End of APawn interface
 
+public:
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 };
+
